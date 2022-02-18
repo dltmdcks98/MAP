f4161558910caf8f226e17589c7df75bdf27096a
dltmdcks98 <dltmdcks98@gmail.com>
2022-02-18 23:31:56 +0900

    FireBase, Google Map 연결

---
+++ app/src/main/java/com/example/map/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.map;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+//https://webnautes.tistory.com/647
+public class MainActivity extends AppCompatActivity
+        implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mMap = googleMap;
+        //경도, 위도
+        LatLng SEOUL = new LatLng(37.56, 126.97);
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(@NonNull Marker marker) {
+                Toast(marker.getTitle());
+                return false;
+            }
+        });
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(SEOUL);
+        markerOptions.title("서울");
+        markerOptions.snippet("한국의 수도");
+        mMap.addMarker(markerOptions);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SEOUL, 10));
+
+
+        LatLng GUNPO = new LatLng(37.3616703, 126.9351741);
+        MarkerOptions markerOptions1 = new MarkerOptions();
+        markerOptions1.position(GUNPO);
+        markerOptions1.title("군포");
+        markerOptions1.snippet("TEST");
+        mMap.addMarker(markerOptions1);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(GUNPO,10));
+    }
+
+    public  void Toast(String str){
+        Toast myToast = Toast.makeText(this.getApplicationContext(),str, Toast.LENGTH_SHORT);
+        myToast.show();
+    }
+
+}
