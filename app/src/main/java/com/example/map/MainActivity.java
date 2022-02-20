1743c227ba3f0496e8117af4a07f80edfdb4d821
dltmdcks98 <dltmdcks98@gmail.com>
2022-02-20 23:07:00 +0900

    firestore Database 연결 테스트

--- app/src/main/java/com/example/map/MainActivity.java
+++ app/src/main/java/com/example/map/MainActivity.java
@@ -14,8 +14,12 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 
 //https://webnautes.tistory.com/647
@@ -27,9 +31,10 @@ public class MainActivity extends AppCompatActivity
     private Button DB_Button;
     //firebase 연결
     //실시간 서버
-      private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-      private DatabaseReference databaseReference = firebaseDatabase.getReference();
+//      private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+//      private DatabaseReference databaseReference = firebaseDatabase.getReference();
     //fire store
+    private  FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +48,8 @@ public class MainActivity extends AppCompatActivity
         DB_Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                databaseReference.child("message").push().setValue("2");
+//                databaseReference.child("message").push().setValue("2");
+                exampleData();
             }
         });
         mapFragment.getMapAsync(this);
@@ -80,56 +86,56 @@ public class MainActivity extends AppCompatActivity
         mMap.addMarker(markerOptions1);
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(GUNPO,10));
     }
-//    public void exampleData() {
-//        // [START example_data]
-//        CollectionReference cities = db.collection("cities");
-//
-//        Map<String, Object> data1 = new HashMap<>();
-//        data1.put("name", "San Francisco");
-//        data1.put("state", "CA");
-//        data1.put("country", "USA");
-//        data1.put("capital", false);
-//        data1.put("population", 860000);
-//        data1.put("regions", Arrays.asList("west_coast", "norcal"));
-//        cities.document("SF").set(data1);
-//
-//        Map<String, Object> data2 = new HashMap<>();
-//        data2.put("name", "Los Angeles");
-//        data2.put("state", "CA");
-//        data2.put("country", "USA");
-//        data2.put("capital", false);
-//        data2.put("population", 3900000);
-//        data2.put("regions", Arrays.asList("west_coast", "socal"));
-//        cities.document("LA").set(data2);
-//
-//        Map<String, Object> data3 = new HashMap<>();
-//        data3.put("name", "Washington D.C.");
-//        data3.put("state", null);
-//        data3.put("country", "USA");
-//        data3.put("capital", true);
-//        data3.put("population", 680000);
-//        data3.put("regions", Arrays.asList("east_coast"));
-//        cities.document("DC").set(data3);
-//
-//        Map<String, Object> data4 = new HashMap<>();
-//        data4.put("name", "Tokyo");
-//        data4.put("state", null);
-//        data4.put("country", "Japan");
-//        data4.put("capital", true);
-//        data4.put("population", 9000000);
-//        data4.put("regions", Arrays.asList("kanto", "honshu"));
-//        cities.document("TOK").set(data4);
-//
-//        Map<String, Object> data5 = new HashMap<>();
-//        data5.put("name", "Beijing");
-//        data5.put("state", null);
-//        data5.put("country", "China");
-//        data5.put("capital", true);
-//        data5.put("population", 21500000);
-//        data5.put("regions", Arrays.asList("jingjinji", "hebei"));
-//        cities.document("BJ").set(data5);
-//        // [END example_data]
-//    }
+    public void exampleData() {
+        // [START example_data]
+        CollectionReference cities = db.collection("cities");
+
+        Map<String, Object> data1 = new HashMap<>();
+        data1.put("name", "San Francisco");
+        data1.put("state", "CA");
+        data1.put("country", "USA");
+        data1.put("capital", false);
+        data1.put("population", 860000);
+        data1.put("regions", Arrays.asList("west_coast", "norcal"));
+        cities.document("SF").set(data1);
+
+        Map<String, Object> data2 = new HashMap<>();
+        data2.put("name", "Los Angeles");
+        data2.put("state", "CA");
+        data2.put("country", "USA");
+        data2.put("capital", false);
+        data2.put("population", 3900000);
+        data2.put("regions", Arrays.asList("west_coast", "socal"));
+        cities.document("LA").set(data2);
+
+        Map<String, Object> data3 = new HashMap<>();
+        data3.put("name", "Washington D.C.");
+        data3.put("state", null);
+        data3.put("country", "USA");
+        data3.put("capital", true);
+        data3.put("population", 680000);
+        data3.put("regions", Arrays.asList("east_coast"));
+        cities.document("DC").set(data3);
+
+        Map<String, Object> data4 = new HashMap<>();
+        data4.put("name", "Tokyo");
+        data4.put("state", null);
+        data4.put("country", "Japan");
+        data4.put("capital", true);
+        data4.put("population", 9000000);
+        data4.put("regions", Arrays.asList("kanto", "honshu"));
+        cities.document("TOK").set(data4);
+
+        Map<String, Object> data5 = new HashMap<>();
+        data5.put("name", "Beijing");
+        data5.put("state", null);
+        data5.put("country", "China");
+        data5.put("capital", true);
+        data5.put("population", 21500000);
+        data5.put("regions", Arrays.asList("jingjinji", "hebei"));
+        cities.document("BJ").set(data5);
+        // [END example_data]
+    }
     public  void Toast(String str){
         Toast myToast = Toast.makeText(this.getApplicationContext(),str, Toast.LENGTH_SHORT);
         myToast.show();
